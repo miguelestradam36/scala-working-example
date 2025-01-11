3ff4c5545bc6edd2633057d1fb24e0b5c4ee174f
miguelestradam36 <miguelestradam36@gmail.com>
2025-01-11 17:28:49 -0600

    Basics of the project saved for later dev.

--- main.scala
+++ main.scala
@@ -1,22 +1,27 @@
-//> using toolkit latest
 //> using dep "com.softwaremill.sttp.client4::core:4.0.0-M22"
-object APIRequest 
+
+import scala.io.Source;
+import scala.util.{Try, Success, Failure};
+import java.io.{File, PrintWriter};
+import sttp.client4.quick.*
+import sttp.client4.Response
+
+object Main
 {
-    def main(args: Array[String]) = {
-      import sttp.client4.quick.*
-      import sttp.client4.Response
-
-      val response: Response[String] = quickRequest
-        .get(uri"https://httpbin.org/get")
-        .send()
-
-      println(response.code)
-      // prints: 200
-
-      println(response.body)
-      // prints some JSON string
-
-      println(response.headers)
-      // prints: Vector
+    def main(args: Array[String]): Unit = {
+      val response: Response[String] = quickRequest.get(uri"https://httpbin.org/get").send()
+      if (response.code.toString == "200"){
+        val file = new File("data.json");
+        val writer = new PrintWriter(file);
+        // Will develop this part later
+        // writer.write(response.headers);
+        // println("Request header saved into JSON file...");
+        writer.write(response.body);
+        println("Request body saved into JSON file...");
+        writer.close();
+        println("Saved the data on the data folder...");
+      } else {
+        println("There was an error during the request")
+      }
     }
 }
